8e5638544b7b1e7727e6a4281192594896b1857d
sasha1637 <82914142+sasha1637@users.noreply.github.com>
2022-11-05 20:45:55 +0300

    123

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe,{autoplay: true});
+let timeVideoLocalStorage=(JSON.parse(localStorage.getItem("videoplayer-current-time")))||0;
+player.setCurrentTime(timeVideoLocalStorage)
+player.on("timeupdate", throttle(onTimePlay,1000)) 
+function onTimePlay(e){
+let timePlayVideo=JSON.stringify(e.seconds)
+localStorage.setItem("videoplayer-current-time",timePlayVideo)
+}
 
+     //  https://lodash.com/docs/4.17.15#throttle
+    //  _.throttle(func, [wait=0], [options={}])
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,19 @@
+// Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email
+//  і message, у яких зберігай поточні значення полів форми. 
+// Нехай ключем для сховища буде рядок "feedback-form-state".
+// Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй 
+// ними поля форми. В іншому випадку поля повинні бути порожніми.
+// Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з полями email,
+//  message та їхніми поточними значеннями.
+// Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд. Для цього додай до проекту
+//  і використовуй бібліотеку lodash.throttle.
+const formRef = document.querySelector('.feedback-form');
+formRef.addEventListener('input',onFormInputSave);
+function onFormInputSave(e){
+    e.preventDefault()
+    console.dir(e.target);
+ 
+    console.dir(e.currentTarget.email.value);
+    console.dir(e.currentTarget.message.value);
 
+}
